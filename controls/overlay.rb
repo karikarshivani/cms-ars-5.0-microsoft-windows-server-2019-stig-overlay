2e7bf9b6c4c8d5e63d81bbffc93cfc7425d87b9b
karikarshivani <karikarshivani@gmail.com>
2022-06-23 16:17:00 -0400

    Added default system categorization
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,9 +1,5 @@
 overlay_controls = input('overlay_controls')
-# puts input('overlay_controls').class
-# puts overlay_controls == []
-# puts overlay_controls.any?
-# puts overlay_controls.empty?
-# puts overlay_controls[0].nil?
+system_categorization = input('system_categorization')
 
 include_controls 'microsoft-windows-server-2019-stig-baseline' do
 
@@ -11,7 +7,7 @@ include_controls 'microsoft-windows-server-2019-stig-baseline' do
     overlay_controls.each do |overlay_control|
       control overlay_control do
         impact 0.0
-        desc "caveat", "Not applicable for this CMS ARS 5.0 overlay, since the related security control is not included in CMS ARS 5.0 for the system categorization of #{ENV["BASELINE"]}"
+        desc "caveat", "Not applicable for this CMS ARS 5.0 overlay, since the related security control is not included in CMS ARS 5.0 for the system categorization of #{system_categorization}"
       end
     end
   end
